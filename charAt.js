2d0a3d68f90c63cbd6f998d033ae5b2886b9d94d
anum882 <anumbad2@gmail.com>
2022-12-02 11:56:34 +0100

    feat:create a new file of a postcard

---
+++ charAt.js
@@ -0,0 +1,15 @@
+// Add your code here
+
+const quoteStart = "'Don't judge each day by the harvest you reap '";
+const quoteEnd = "but by the seed that you plant"
+finalQuote = quoteStart + quoteEnd
+
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+const para1 = document.createElement('p');
+para1.textContent = finalQuote;
+
+section.appendChild(para1);
+    
